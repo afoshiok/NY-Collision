4f54c637da8e46eda88ccd001832f764ee3a4eef
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-07-11 12:14:05 -0500

    'Collision' created in Duckdb

--- main.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-
-#Testing Duckdb, making sure it's connected
-con = duckdb.connect("NY_Collisions.db")
-table_test = con.sql("SELECT * FROM './NY_Collisions.csv' LIMIT 10;")
-table_test.show()
-
